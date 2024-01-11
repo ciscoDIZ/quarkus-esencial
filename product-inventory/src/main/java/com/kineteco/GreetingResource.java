4fd1cf24b23021a60c45489d7af8ff83790a4b63
francisco de asís domínguez iceta <ciscoadiz@gmail.com>
2024-01-11 17:52:34 +0000

    Using Quarkus dev mode and hot deploying changes

--- product-inventory/src/main/java/com/kineteco/GreetingResource.java
+++ product-inventory/src/main/java/com/kineteco/GreetingResource.java
@@ -1,5 +1,7 @@
 package com.kineteco;
 
+import com.kineteco.model.ProductInventory;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -9,8 +11,10 @@ import javax.ws.rs.core.MediaType;
 public class GreetingResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Produces(MediaType.APPLICATION_JSON)
+    public ProductInventory hello() {
+        ProductInventory productInventory = new ProductInventory();
+        productInventory.setSku("123");
+        return productInventory;
     }
 }
